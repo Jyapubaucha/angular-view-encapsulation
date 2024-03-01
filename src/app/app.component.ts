6fbc9aad1bebd8cfa5af73ee1a764cbc2a924c30
Jyapubaucha <Jyapubaucha21@gmail.com>
2024-03-01 15:12:18 +0545

    Learned viewEncasulation.none.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // if ViewEncapsulation.None is coded the css property will be apply
+  // to the child parent too.
+  
+  // encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'angular-view-encapsulation';
